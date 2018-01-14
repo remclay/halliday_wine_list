cbc7fceaf55b53b72d4ed106dbc20dcfafaa8dff
Remmy Clay <remmy.lauren@gmail.com>
2018-01-14 21:02:42 +0000

    Begin coding HallidayWineList::CLI

--- lib/halliday_wine_list/cli.rb
+++ lib/halliday_wine_list/cli.rb
@@ -3,6 +3,26 @@ class HallidayWineList::CLI
 
   def call
     puts "James Halliday Top 100 Wines 2017"
+    list_categories
   end
+
+  def list_categories
+    puts "--------------------------------------------"
+    puts "1 - Best Red Wine under $25 (20 wines selected)"
+    puts "2 - Best White Wine under $20 (20 wines selected)"
+    puts "3 - Best Red Wine over $25 (20 wines selected)"
+    puts "4 - Best White Wine under $20 (20 wines selected)"
+    puts "5 - Best Champagne (12 wines selected)"
+    puts "6 - Best Sparkling (8 wines selected)"
+    puts "--------------------------------------------"
+    choose_category
+    #check out here docs to potentially improve this method
+  end
+
+  def choose_category
+    puts "Please choose a category to view. Enter 1-6."
+    input = gets.strip
+  end
+
 
 end
