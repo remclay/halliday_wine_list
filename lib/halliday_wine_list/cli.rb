2a7d5a7bdf6d9ff137271bcdfd02efffb82ad78f
Remmy Clay <remmy.lauren@gmail.com>
2018-01-23 19:23:52 +0000

    Amend puts messages in HallidayWineList::CLI and rename reveal_wines method. Add user input option in HallidayWineList::CLI #next_steps

--- lib/halliday_wine_list/cli.rb
+++ lib/halliday_wine_list/cli.rb
@@ -10,47 +10,49 @@ class HallidayWineList::CLI
   end
 
   def choose_category
+    categories = HallidayWineList::Category.all_categories
     puts ""
     puts "Categories:"
     list_categories
     puts ""
-    puts "Choose a category (1-4) to view wines in that category."
-    puts "Type list to see all wines or type exit."
+    puts "Choose a category (1-#{categories.count}) to view wines in that category, type 'list' to see all wines or type exit to leave."
     @input = gets.strip.downcase
-    categories = HallidayWineList::Category.all_categories
     if users_choice >= 0 && users_choice < (categories.count)
       puts "Showing: #{categories[users_choice].name}."
       puts "--------------------------------------------"
-      reveal_wines
-      next_steps
+      print_category_wines
     elsif @input == "exit"
       goodbye
     elsif @input == "list"
       print_all_wines
-      next_steps
     else
-      puts "Invalid input."
+      puts "Sorry, I don't recognise that input. Please enter a valid input."
       choose_category
     end
+    next_steps
   end
 
   def next_steps
     puts ""
-    puts "What would you like to do next?"
+    puts "What would you like to do next? (1-3)"
     puts "1 View categories"
-    puts "2 Exit"
+    puts "2 List all wines"
+    puts "3 Exit"
     input = gets.strip.to_i
     if input == 1
       choose_category
     elsif input == 2
+      print_all_wines
+      next_steps
+    elsif input == 3
       goodbye
     else
-      puts "Invalid input"
+      puts "Sorry, I don't recognise that input. Please enter a valid input (1-3)."
       next_steps
     end
   end
 
-  def reveal_wines
+  def print_category_wines
     HallidayWineList::Category.all_categories[users_choice].print_wines
   end
 
